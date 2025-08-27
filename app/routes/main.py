1a0e735f71c2056df2bd05379e3c4d82b2054023
milckywayy <01178835@pw.edu.pl>
2025-08-27 04:51:23 +0200

    Add encironments list

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
+from sqlalchemy.orm import joinedload
 
-from app.models import Cluster
+from app.models import Cluster, Environment
 
 main_bp = Blueprint("main", __name__)
 
@@ -9,6 +10,22 @@ main_bp = Blueprint("main", __name__)
 def index():
     clusters = Cluster.query.join(Cluster.environments).all()
     return render_template("index.html", clusters=clusters)
+
+
+@main_bp.route("/environments")
+def environments():
+    existing_environments = (
+        Environment.query.options(
+            joinedload(Environment.docker),
+            joinedload(Environment.vm),
+        )
+        .order_by(Environment.name.asc())
+        .all()
+    )
+
+    return render_template(
+        "environments.html", existing_environments=existing_environments
+    )
 
 
 @main_bp.route("/base")
